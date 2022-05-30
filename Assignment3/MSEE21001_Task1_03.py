1c9485538bc8ef9c2464291b9888aee989090260
abdulbasit0369 <abdul.basit@itu.edu.pk>
2022-05-30 18:40:25 +0500

    comit

--- Assignment3/MSEE21001_Task1_03.py
+++ Assignment3/MSEE21001_Task1_03.py
@@ -108,15 +108,15 @@ class MobileNet(nn.Module):
 		self.inChannels = num_filter
 
 		self.model = nn.Sequential(
-           MobileNet_block(num_filter, 64, 1),
-           MobileNet_block(64, 128, 2),
-           MobileNet_block(128, 128, 1),
-           MobileNet_block(128, 256, 2),
-           MobileNet_block(256, 512, 2),
-           MobileNet_block(512, 512, 1),
-           MobileNet_block(512, 1024, 2),
-           MobileNet_block(1024, 1024, 1),
-        )
+		   MobileNet_block(num_filter, 64, 1),
+		   MobileNet_block(64, 128, 2),
+		   MobileNet_block(128, 128, 1),
+		   MobileNet_block(128, 256, 2),
+		   MobileNet_block(256, 512, 2),
+		   MobileNet_block(512, 512, 1),
+		   MobileNet_block(512, 1024, 2),
+		   MobileNet_block(1024, 1024, 1),
+		)
 
 		self.avgpool = nn.AdaptiveAvgPool2d(output_size=(1, 1))
 		self.fc = nn.Sequential(
@@ -137,8 +137,8 @@ class MobileNet(nn.Module):
 
 class helper_functions(MobileNet):
 	
-	def train(self, train_loader, valid_loader, criterion, optimizer, training_epochs):
-		model = self
+	def train(self,model, train_loader, valid_loader, criterion, optimizer, training_epochs):
+		model = model
 		trainLoss=[]
 		trainAcc=[]
 		validLoss=[]
@@ -208,7 +208,7 @@ class helper_functions(MobileNet):
 			
 			#if lr_decay_scheduler is not None:
 			#	lr_decay_scheduler.step()
-			min_valid_loss = model.EarlyStopping(min_valid_loss, valid_loss/len(valid_loader))
+			min_valid_loss = self.EarlyStopping(model, min_valid_loss, valid_loss/len(valid_loader))
 
 			print(f'Epoch {e+1} Training Loss: {train_loss / len(train_loader)} \t\t Validation Loss: {valid_loss / len(valid_loader)}')
 			
@@ -216,10 +216,10 @@ class helper_functions(MobileNet):
 			
 		return model,trainLoss, validLoss, trainAcc, validAcc
 
-	def EarlyStopping(self,min_valid_loss, valid_loss):
+	def EarlyStopping(self,model ,min_valid_loss, valid_loss):
 		min_valid_loss =min_valid_loss
 		if min_valid_loss > valid_loss:
-			model=self
+			model = model
 			#print(f'Validation Loss Decreased({min_valid_loss:.6f}--->{valid_loss:.6f}) \t Saving The Model')
 			min_valid_loss = valid_loss
 			
@@ -262,15 +262,16 @@ class helper_functions(MobileNet):
 		)
 		plt.title(plot_title)
 		plt.show()
-	def predict(self, testX):
+	def predict(self,model, testX):
 		# predict the value of testX
-		test_pred=self(testX)
+		test_pred=model(testX)
 		
 		
 		return test_pred  #predictions
 
-	def predict_digit(self,testX):
-		test_pred=self(testX)
+	def predict_digit(self,model,testX):
+		model = model
+		test_pred=model(testX)
 		_, predicted = test_pred.max(1)
 		return predicted
 
@@ -361,8 +362,8 @@ learning_rate = 0.05
 # Initializing the model 
 ######################################
 
-model = MobileNet()
-summary(model, (1, 28, 28))
+MobileNetmodel = MobileNet()
+summary(MobileNetmodel, (1, 28, 28))
 # the helper_functions class inherits the MobileNet
 #So by constructing the helper_function class we initialize the model too
 model = helper_functions()
@@ -372,16 +373,11 @@ model = helper_functions()
 ######################################
 criterion = nn.CrossEntropyLoss()
 #optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9)
-optimizer = optim.Adam(model.parameters(),lr=learning_rate,betas=(0.9,0.999),eps=1e-08,weight_decay=0,amsgrad=False)
+optimizer = optim.Adam(MobileNetmodel.parameters(),lr=learning_rate,betas=(0.9,0.999),eps=1e-08,weight_decay=0,amsgrad=False)
 
 if torch.cuda.is_available():
-		model.cuda()
+		MobileNetmodel.cuda()
 		criterion.cuda()
-##################################################################
-##                        Learning rate decay                   ##
-##################################################################
-#decayRate = 0.96
-#lr_decay_scheduler = optim.lr_scheduler.ExponentialLR(optimizer=optimizer, gamma=decayRate)
 
 ##################################################################
 ##                        Training The model                    ##
@@ -389,7 +385,7 @@ if torch.cuda.is_available():
 # The EarlyStoping is implemented in the class neural network
 #  
 
-model, trainLoss, validLoss, trainAcc, validAcc=model.train(train_loader, valid_loader, criterion, optimizer, epochs)
+MobileNetmodel, trainLoss, validLoss, trainAcc, validAcc=model.train(MobileNetmodel, train_loader, valid_loader, criterion, optimizer, epochs)
 
 #saving the model
 #torch.save(model.state_dict(),'saved_model.pth') 
@@ -422,11 +418,11 @@ if plot_err:
 ###################################################################
 
 # create class object
-
-trainedModel = model
+helper_func = helper_functions()
+trainedModel = MobileNetmodel
 trainedModel.load_state_dict(torch.load('saved_model.pth'))
 trainedModel.cpu()
-
+trainedModel.eval()
 ###############################################################
 #                  test data analysis
 ###############################################################
@@ -436,14 +432,14 @@ tlabel=0
 for data, labels in test_loader:
 	tdata=data
 	tlabel=labels
-
+#print(tdata[0])
 ################	
 #predict a digit
 ################
-test_pred = trainedModel.predict(tdata)
-testAcc = trainedModel.accuracy(labels,test_pred)
+test_pred = helper_func.predict(trainedModel, tdata)
+testAcc = helper_func.accuracy( labels,test_pred)
 print(torch.unsqueeze(tdata[0], 0).shape)
-print(trainedModel.predict_digit(torch.unsqueeze(tdata[0], 0)))
+print(helper_func.predict_digit(trainedModel, torch.unsqueeze(tdata[0], 0)))
 
 ################
 # Accuracy
@@ -454,14 +450,14 @@ print("test Accuracy = ",testAcc)
 # precision Recall and F1-score
 ################
 
-print(trainedModel.precision_recall_f1score(labels, test_pred))
+print(helper_func.precision_recall_f1score(labels, test_pred))
 
 
 ################
 #Confusion Matrix calculation
 ################
 
-trainedModel.confusion_Mat(tlabel, test_pred,'Test Data Confusion Matrix')
+helper_func.confusion_Mat(tlabel, test_pred,'Test Data Confusion Matrix')
 
  # %%
 ###############################################################
@@ -511,62 +507,61 @@ model_weights = [] # we will save the conv layer weights in this list
 conv_layers = [] # we will save the 49 conv layers in this list
 # get all the model children as list
 model_children = list(trainedModel.children())
-print(model_children)
-
 
 # counter to keep count of the conv layers
 counter = 0 
 
 # append all the conv layers and their respective weights to the list
 for i in range(len(model_children)):
+	
 	if type(model_children[i]) == nn.Conv2d:
 		counter += 1
 		model_weights.append(model_children[i].weight)
 		conv_layers.append(model_children[i])
 	elif type(model_children[i]) == nn.Sequential:
+		
 		for j in range(len(model_children[i])):
+			
 			for child in model_children[i][j].children():
-				if type(child) == nn.Conv2d:
-					counter += 1
-					model_weights.append(child.weight)
-					conv_layers.append(child)
+				
+				for ch in child.children():
+					for chi in ch.children():
+						if type(chi) == nn.Conv2d:
+							counter += 1
+							model_weights.append(chi.weight)
+							conv_layers.append(chi)
+							
+				
 print(f"Total convolutional layers: {counter}")
 
-# take a look at the conv layers and the respective weights
-for weight, conv in zip(model_weights, conv_layers):
-	# print(f"WEIGHT: {weight} \nSHAPE: {weight.shape}")
-	print(f"CONV: {conv} ====> SHAPE: {weight.shape}")
-# visualize the first conv layer filters
 plt.figure(figsize=(20, 20))
-for i, filter in enumerate(model_weights[1]):
-	plt.subplot(10, 10, i+1) # (8, 8) because in conv0 we have 7x7 filters and total of 64 (see printed shapes)
+# Printing the convolutional layer so that we can see the filter size and number of filters
+for i in range(len(conv_layers)):
+	print(conv_layers[i])
+# visualizing the final convolution layer filters
+# we have 1024 of them i only plotted first 100 of them
+for i, filter in enumerate(model_weights[14]):
+	plt.subplot(10, 10, i+1) # (3, 3) because in conv14 we have 3x3 filters and total of 1024
 	plt.imshow(filter[0, :, :].detach(), cmap='gray')
 	plt.axis('off')
+	if i == 99:
+		break
 	plt.savefig('filter.png')
+	
 plt.show()
-# %%
-#######################
-# visualizing output of final convolutional layer
-#######################
-outputs= []
-def hook(module, input, output):
-	outputs.append(output)
 
 
-#trainedModel.layer4[0].conv2.register_forward_hook(hook)
-#out = res50_model(res)
-#out = res50_model(res1)
-#print(outputs)
+
 # %%
 ###############################################################
 #     tSNE plots
 #test data tsne plot 
 
-data = tdata.reshape(tdata.shape[0], -1)
+testdata = tdata.reshape(tdata.shape[0], -1)
 data_pred = predicted
 
 
-trainedModel.tSNE(data,data_pred,'test data tsne plot')
+helper_func.tSNE(testdata,data_pred,'test data tsne plot')
 
 
 
